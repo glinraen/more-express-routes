e0bea7836ab1e03f5679b02113493ce944603f1c
GLINRAEN <MISTRESS.WYN@GMAIL.COM>
2016-05-23 21:09:39 -0700

    add exercises

--- quick_example/index.js
+++ quick_example/index.js
@@ -9,8 +9,6 @@ app.get("/", function (req, res) {
     res.send("Hello World");
 });
 
-
-
 var burgers = [
                 "Hamburger",
                 "Cheese Burger",
@@ -29,7 +27,7 @@ app.get("/", function (req, res) {
 
 app.get("/color/:choice", function(req, res){
   var choice = req.params.choice;
-  res.send("Your color is: " + choice);
+  res.send("The color you picked is: " + choice);
 });
 
 app.get("/thank", function (req, res) {
@@ -40,6 +38,24 @@ app.get("/thank", function (req, res) {
 app.get("/burgers", function (req, res) {
     //send all the burgers     
     res.send(burgers.join(", "));
+});
+
+app.get("/tacos", function (req, res) {
+    //send all the tacos           
+    res.send(tacos.join(", "));
+});
+
+app.get("/taco/:index", function (req, res) {
+	var index = req.params.index;
+	var pick = tacos[index] || "Not a choice. Try again.";
+	//console.log("hit");
+	res.send(pick);
+});
+
+app.get("/burger/:index", function(req, res) {
+	var index = req.params.index;
+	var pick = burgers[index] || "Not a choice. Try again.";
+	res.send(pick);
 });
 
 app.get("/tacos", function (req, res) {
